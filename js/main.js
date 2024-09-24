705500ef52a08cc44f63ebb82a6dde3f1b5a4b08
noosrat-jahan <noosratmeem@gmail.com>
2024-09-24 19:15:47 +0600

    common function is added to reduce code redundancy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
+// Common Function start
+
+function getInputFieldValueById(id){
+    return parseFloat(document.getElementById(id).value)
+}
+
+function getTextFieldValueById(id){
+    return parseFloat(document.getElementById(id).innerText)
+}
+
+function historyTabAttribute(id){
+    document.getElementById(id).classList.add('bg-[#B4F461]', 'hover:bg-[#a5e651]')
+    document.getElementById(id).classList.remove('border', 'border-[#1111114D]', 'text-[#111111B3]')
+}
+
+function donationTabAttribute(id){
+    document.getElementById(id).classList.remove('bg-[#B4F461]', 'hover:bg-[#a5e651]', 'text-[rgb(17,17,17)]')
+    document.getElementById(id).classList.add('text-[#111111B3]','border', 'border-[#1111114D]','hover:bg-[#5452524d]')
+}
+
+function historyContainer(donationArea, title){
+    const historyItem = document.createElement('div')
+    historyItem.innerHTML = `
+    <div class="border border-[#1111111A] rounded-lg p-6 mb-3">
+        <p class="font-bold mb-3">${donationArea} ${title}</p>
+        <p class="text-[#111111B3]">Date: ${new Date()}</p>
+    </div>
+    `
+    const historyContainer = document.getElementById('history-page')
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+}
+// common function end
+
+
+
 document.getElementById('btn-doante-1').addEventListener('click', function(){
 
-    const donationNoakhali = parseFloat(document.getElementById('donation-amount-noakhali').value)
-    const totalForNoakhali = parseFloat(document.getElementById('noakhali-total').innerText)
-    const BalanceTop = parseFloat(document.getElementById('balance-top').innerText)
+    const donationNoakhali = getInputFieldValueById('donation-amount-noakhali')
+    const totalForNoakhali = getTextFieldValueById('noakhali-total')
+    const BalanceTop = getTextFieldValueById('balance-top')
 
     if(donationNoakhali <= 0 || isNaN(donationNoakhali)){
         alert('Please enter a valid amount..')
@@ -13,7 +48,7 @@ document.getElementById('btn-doante-1').addEventListener('click', function(){
     const newBalance = BalanceTop - donationNoakhali
 
     if(BalanceTop < donationNoakhali){
-        alert("Insufficient Balance...")
+        alert("Attempt Failed due to Insufficient Balance.")
         return
     }
 
@@ -22,24 +57,15 @@ document.getElementById('btn-doante-1').addEventListener('click', function(){
     document.getElementById('balance-top').innerText = newBalance
     my_modal_1.showModal()
     
-    const historyItem = document.createElement('div')
-    historyItem.innerHTML = `
-    <div class="border border-[#1111111A] rounded-lg p-6 mb-3">
-        <p class="font-bold mb-3">${donationNoakhali} Taka is Donated for Flood at Noakhali, Bangladesh</p>
-        <p class="text-[#111111B3]">Date: ${new Date()}</p>
-    </div>
-    `
-    const historyContainer = document.getElementById('history-page')
-    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
-
+    historyContainer(donationNoakhali, 'Taka is Donated for Flood at Noakhali, Bangladesh')
 })
 
 
 document.getElementById('btn-doante-2').addEventListener('click', function(){
-    const donationFeni = parseFloat(document.getElementById('donation-amount-feni').value)
-    const totalForFeni = parseFloat(document.getElementById('Feni-total').innerText)
-    const BalanceTop = parseFloat(document.getElementById('balance-top').innerText)
 
+    const donationFeni = getInputFieldValueById('donation-amount-feni')
+    const totalForFeni = getTextFieldValueById('Feni-total')
+    const BalanceTop = getTextFieldValueById('balance-top')
 
     if(donationFeni <= 0 || isNaN(donationFeni)){
         alert('Please enter a valid amount..')
@@ -50,7 +76,7 @@ document.getElementById('btn-doante-2').addEventListener('click', function(){
     const newBalance = BalanceTop - donationFeni
 
     if(BalanceTop < donationFeni){
-        alert("Insufficient Balance...")
+        alert("Attempt Failed due to Insufficient Balance.")
         return
     }
 
@@ -59,34 +85,25 @@ document.getElementById('btn-doante-2').addEventListener('click', function(){
     document.getElementById('balance-top').innerText = newBalance
     my_modal_2.showModal()
 
-    const historyItem = document.createElement('div')
-    historyItem.innerHTML = `
-    <div class="border border-[#1111111A] rounded-lg p-6 mb-3">
-        <p class="font-bold mb-3">${donationFeni} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
-        <p class="text-[#111111B3]">Date: ${new Date()}</p>
-    </div>
-    `
-    const historyContainer = document.getElementById('history-page')
-    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+    historyContainer(donationFeni, 'Taka is Donated for Flood Relief in Feni,Bangladesh')
 })
 
 
 document.getElementById('btn-doante-3').addEventListener('click', function(){
-    const donationQuota = parseFloat(document.getElementById('donation-amount-quota').value)
-    const totalForQuota = parseFloat(document.getElementById('quota-injured-donation-total').innerText)
-    const BalanceTop = parseFloat(document.getElementById('balance-top').innerText)
+    const donationQuota = getInputFieldValueById('donation-amount-quota')
+    const totalForQuota = getTextFieldValueById('quota-injured-donation-total')
+    const BalanceTop = getTextFieldValueById('balance-top')
 
     if(donationQuota <= 0 || isNaN(donationQuota)){
         alert('Please enter a valid amount..')
         return
     }
 
-
     const newAmount = donationQuota + totalForQuota
     const newBalance = BalanceTop - donationQuota
 
     if(BalanceTop < donationQuota){
-        alert("Insufficient Balance...")
+        alert("Attempt Failed due to Insufficient Balance.")
         return
     }
 
@@ -95,57 +112,33 @@ document.getElementById('btn-doante-3').addEventListener('click', function(){
     document.getElementById('balance-top').innerText = newBalance
     my_modal_3.showModal()
 
-    const historyItem = document.createElement('div')
-    historyItem.innerHTML = `
-    <div class="border border-[#1111111A] rounded-lg p-6 mb-3">
-        <p class="font-bold mb-3">${donationQuota} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
-        <p class="text-[#111111B3]">Date: ${new Date()}</p>
-    </div>
-    `
-    const historyContainer = document.getElementById('history-page')
-    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+    historyContainer(donationQuota, 'Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh')
 })
+
 
 document.getElementById('history-tab').addEventListener('click', function(){
 
-    const historyTab = document.getElementById('history-tab')
-    historyTab.classList.add('bg-[#B4F461]', 'hover:bg-[#a5e651]')
-    historyTab.classList.remove('border', 'border-[#1111114D]', 'text-[#111111B3]')
-
-    const donationTab = document.getElementById('donation-tab')
-
-    donationTab.classList.remove('bg-[#B4F461]', 'hover:bg-[#a5e651]', 'text-[rgb(17,17,17)]')
-    donationTab.classList.add('text-[#111111B3]','border', 'border-[#1111114D]','hover:bg-[#5452524d]')
+    historyTabAttribute('history-tab')
+    
+    donationTabAttribute('donation-tab')    
     
    const DonationPage = document.getElementById('donation-page')
    DonationPage.classList.add('hidden')
 
    const historyPage = document.getElementById('history-page')
    historyPage.classList.remove('hidden')
-
     
 })
 
 document.getElementById('donation-tab').addEventListener('click', function(){
-
-    const donationTab = document.getElementById('donation-tab')
-    const historyTab = document.getElementById('history-tab')     
-
    
-    donationTab.classList.add('bg-[#B4F461]', 'hover:bg-[#a5e651]')
-    donationTab.classList.remove('border', 'border-[#1111114D]', 'text-[#111111B3]')
-
-    historyTab.classList.add('border', 'border-[#1111114D]', 'text-[#111111B3]')
-    historyTab.classList.remove('bg-[#B4F461]', 'hover:bg-[#a5e651]')
-    
+    historyTabAttribute('donation-tab')
+    donationTabAttribute('history-tab')       
 
     const DonationPage = document.getElementById('donation-page')
     DonationPage.classList.remove('hidden')
 
     const historyPage = document.getElementById('history-page')
-    historyPage.classList.add('hidden')
-
-    
-    
+    historyPage.classList.add('hidden')    
 })
- 
+
